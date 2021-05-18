ac1261b6ed510461f4dc08226f4025991d3d80ef
Mohammed Rabeeh <mohammadrabeeh@gmail.com>
2021-05-18 23:36:52 +0530

    Change from proxy to manual request forwarding

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
 app.use(morgan('combined'));
 
-app.use(
-  '/api',
-  createProxyMiddleware({
-    target: 'https://cdn-api.co-vin.in',
-    changeOrigin: true,
-  }),
-);
+const cowinURL = 'https://cdn-api.co-vin.in';
+
+app.use('/api', async (req, res) => {
+  const url = req.originalUrl;
+  try {
+    const response = await axios.get(`${cowinURL}${url}`, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
+      },
+    });
+    res.json({ data: response.data });
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 const PORT = process.env.PORT || 7744;
 
